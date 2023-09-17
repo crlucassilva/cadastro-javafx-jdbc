b4913f817c9402f0792939f16b90ca4e22a2008f
Lucas Silva <lucas.cristiano96@gmail.com>
2023-09-17 16:23:09 -0300

    Saving Seller

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -69,7 +69,7 @@ public class SellerDaoJDBC implements SellerDao {
 		try {
 			st = conn.prepareStatement(
 					"UPDATE seller "
-					+ "(SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? )"
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
 					+ "WHERE Id = ?");
 			
 			st.setString(1, obj.getName());
